8e87eee85bbaf6d4e40ac28075cb2485eef4a2e7
Niccolo Zuppichini <zuppin@usi.ch>
2022-08-11 20:42:29 +0200

    Correct bed size Ender 3

--- code/Marlin/Configuration.h
+++ code/Marlin/Configuration.h
@@ -1382,8 +1382,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 235
+#define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
